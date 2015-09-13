385b02f46016c47918218e3b54aa750f0f139554
Jacob Henke <jacob@distracted.io>
2015-09-12 20:59:54 -0500

    install the wp-cli

--- apc.bash
+++ apc.bash
@@ -4,4 +4,6 @@ then
   yes '' | pecl install apc
   echo "extension=apc.so" > /etc/php.d/apc.ini
   systemctl restart  httpd.service
+  ln -s /data/public_html/ /home/vagrant/public_html
+  chown -h vagrant:vagrant /home/vagrant/public_html
 fi
